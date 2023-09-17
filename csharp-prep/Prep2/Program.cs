3f2dff5999570e87ba1eaa40c44aee8d9c8d3849
wakatsuki23 <lesliewakatsuki@gmail.com>
2023-09-17 15:44:08 -0700

    finished the grade program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade in Percentage? ");
+        string percent = Console.ReadLine();
+        int grade = int.Parse(percent);
+        // int A = 90;
+        // int B = 80;
+        // int C = 70;
+        // int D = 60;
+        // int F = 59;
+        string letter ="";
+
+        if (grade >= 90)
+
+        {
+            letter = "A";    
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade>= 60)
+        {
+            letter= "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine ($"Your grade is {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine ("You passed! Congrats!");
+        }
+        else
+        {
+            Console.WriteLine ("Better luck next time! ");
+        }
     }
 }
